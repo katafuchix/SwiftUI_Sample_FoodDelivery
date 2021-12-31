630c0987872b96a3738988f899792544554d29e5
katafuchix <katafuchix@gmail.com>
2022-01-01 01:50:55 +0900

    Initial Commit

---
+++ SwiftUI_Sample_FoodDelivery/SwiftUI_Sample_FoodDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Sample_FoodDeliveryApp.swift
+//  SwiftUI_Sample_FoodDelivery
+//
+//  Created by cano on 2022/01/01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Sample_FoodDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
